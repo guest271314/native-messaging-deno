02b716b2eeeb7a535b1873ea0bef99d78be06242
guest271314 <guest271314@gmail.com>
2023-01-01 10:46:04 -0800

    Use BufReader
    https://github.com/denoland/deno/discussions/17236#discussioncomment-4566541
    
    Deno.stdin.read(buffer) is not guaranteed to read exactly buffer.byteLength bytes, and thus the first message was not fully read. BufReader from std provides a convenient way to ensure that the whole buffer is filled.z

--- nm_deno.js
+++ nm_deno.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env -S ./deno run --v8-flags="--expose-gc,--jitless"
 // Deno Native Messaging host
 // guest271314, 10-5-2022
+import { BufReader } from "https://deno.land/std@0.170.0/io/buf_reader.ts";
+import { BufWriter } from "https://deno.land/std@0.170.0/io/buf_writer.ts";
+
+const stdin = new BufReader(Deno.stdin);
+const stdout = new BufWriter(Deno.stdout);
+
 async function getMessage() {
   const header = new Uint32Array(1);
-  await Deno.stdin.read(header);
-  const output = new Uint8Array(header[0]);
-  await Deno.stdin.read(output);
-  return output;
+  await stdin.readFull(new Uint8Array(header.buffer));
+  const message = new Uint8Array(header[0]);
+  await stdin.readFull(message);
+  return message;
 }
 
 async function sendMessage(message) {
-  const header = Uint32Array.from(
-    {
-      length: 4,
-    },
-    (_, index) => (message.length >> (index * 8)) & 0xff
-  );
-  const output = new Uint8Array(header.length + message.length);
-  output.set(header, 0);
-  output.set(message, 4);
-  await Deno.stdout.write(output.buffer);
+  const header = new Uint32Array([message.length]);
+  await stdout.write(new Uint8Array(header.buffer));
+  await stdout.write(message);
+  await stdout.flush();
 }
 
 async function main() {
