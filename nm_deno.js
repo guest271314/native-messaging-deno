e33eb46cb98c41947accf1e8940c29400c7e8a52
guest271314 <guest271314@gmail.com>
2024-02-11 15:20:22 -0800

    Stream STDIN, read/write to resizable ArrayBuffer

--- nm_deno.js
+++ nm_deno.js
@@ -2,22 +2,38 @@
 // Deno Native Messaging host
 // guest271314, 10-5-2022
 
-// https://github.com/denoland/deno/discussions/17236#discussioncomment-4566134
-// https://github.com/saghul/txiki.js/blob/master/src/js/core/tjs/eval-stdin.js
-async function readFullAsync(length) {
-  const buffer = new Uint8Array(65536);
-  const data = [];
-  let n = null;
-  while (data.length < length && (n = await Deno.stdin.read(buffer))) {
-    data.push(...buffer.subarray(0, n));
-  }
-  return new Uint8Array(data);
+const encoder = new TextEncoder();
+
+function encodeMessage(message) {
+  return encoder.encode(JSON.stringify(message));
 }
 
-async function getMessage() {
-  const header = new Uint32Array(1);
-  await Deno.stdin.read(new Uint8Array(header.buffer));
-  return readFullAsync(header[0]);
+async function* getMessage() {
+  const buffer = new ArrayBuffer(0, { maxByteLength: 1024**2 });
+  const view = new DataView(buffer);
+  let messageLength = 0;
+  let readOffset = 0;
+  for await (let message of Deno.stdin.readable) {
+    if (buffer.byteLength === 0) {
+      buffer.resize(4);
+      for (let i = 0; i < 4; i++) {
+        view.setUint8(i, message[i]);
+      }
+      messageLength = view.getUint32(0, true);
+      message = message.subarray(4);
+      buffer.resize(0);
+    }
+    buffer.resize(buffer.byteLength + message.length);
+    for (let i = 0; i < message.length; i++, readOffset++) {
+      view.setUint8(readOffset, message[i]);
+    }
+    if (buffer.byteLength === messageLength) {
+      yield new Uint8Array(buffer);
+      messageLength = 0;
+      readOffset = 0;
+      buffer.resize(0);
+    }
+  }
 }
 
 async function sendMessage(message) {
@@ -26,16 +42,10 @@ async function sendMessage(message) {
   await Deno.stdout.write(message);
 }
 
-async function main() {
-  while (true) {
-    const message = await getMessage();
-    await sendMessage(message);
-    gc();
-  }
-}
-
 try {
-  main();
+  for await (const message of getMessage()) {
+    await sendMessage(message);
+  }
 } catch (e) {
   Deno.exit();
 }
